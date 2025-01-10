f352a4f2cd9f7a319fb08fe4fd8810c46b5fed3f
RaffiDzakwan <raffidzakwanc@gmail.com>
2025-01-10 08:49:29 +0700

    Latihan meeting 5

---
+++ Javascript/Meeting5/latihanM5.js
@@ -0,0 +1,21 @@
+
+// Nomor 1.
+let dressSize = 45;
+console.log ( typeof dressSize)
+
+// Mengubah tipe data ke string
+dressSize = String (dressSize);
+console.log (typeof dressSize)
+
+// Mengubah tipe data menjadi number
+dressSize = Number (dressSize);
+console.log (typeof dressSize)
+
+
+// Nomor 2.
+let isLight = true;
+console.log (typeof isLight)
+
+// Mengubah tipe data menjadi string
+isLight = String (isLight);
+console.log (typeof isLight)
